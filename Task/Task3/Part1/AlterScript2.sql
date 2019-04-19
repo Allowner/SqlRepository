ef1796bca9bef349b374e7827149e538d0035579
Allowner <se1999va@tut.by>
2019-04-19 12:04:16 +0300

    Change

--- Task/Task3/Part1/AlterScript2.sql
+++ Task/Task3/Part1/AlterScript2.sql
@@ -1,11 +1,11 @@
 GO 
-IF EXISTS(SELECT 1 FROM sys.tables WHERE [name] = N'[dbo.Region]')
+IF EXISTS(SELECT 1 FROM sys.tables WHERE [name] = 'Region')
 BEGIN
 	EXEC sp_rename 'dbo.Region', 'Regions';
 END;
 
 GO 
-IF EXISTS(SELECT 1 FROM sys.columns WHERE [name] = N'[CreationDate]')
+IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE [name] = 'CreationDate')
 BEGIN
     ALTER TABLE [dbo].[Customers] 
 	ADD [CreationDate] DATE NULL;
